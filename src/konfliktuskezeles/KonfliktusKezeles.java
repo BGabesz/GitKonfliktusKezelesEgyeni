9f35f1e425bb3f720c95c5f9b74f96bdf790e945
BGabesz <bogdan.gabesz@gmail.com>
2022-03-11 14:50:09 +0100

    elso nev inicializalasa

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -1,8 +1,9 @@
 package konfliktuskezeles;
 public class KonfliktusKezeles {
     public static void main(String[] args) {
-        String nev1;
+        String nev1="BogdánGábor";
         String nev2;
+        String nev3;
     }
     
 }
